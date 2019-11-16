c11a3310fe6879ecfe34849d960522ba68932d5b
yuri <yuri.h.silva@kroton.com.br>
2019-11-16 16:25:06 -0300

    models

---
+++ app/Models/Category.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Category extends Model {
+
+    image() {
+        return this.belongsTo('App/Models/Image')
+    }
+
+    products() {
+        return this.belongsToMany('App/Models/Product')
+    }
+}
+
+module.exports = Category
---
+++ app/Models/Product.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Product extends Model {
+
+    image() {
+        return this.belongsTo('App/Models/Image')
+    }
+
+    images() {
+        return this.belongsToMany('App/Models/Image')
+    }
+
+    categories() {
+        return this.belongsToMany('App/Models/Category')
+    }
+
+    coupons() {
+        return this.belongsToMany('App/Models/Coupon')
+    }
+}
+
+module.exports = Product
