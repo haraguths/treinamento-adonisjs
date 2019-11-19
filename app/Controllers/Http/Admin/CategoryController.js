49589389e992a63c8f763cfcf038595dbfa9e90f
yuri <yuri.h.silva@kroton.com.br>
2019-11-19 12:51:37 -0300

    Implementa controller categoria

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -10,7 +10,7 @@ class CategoryController {
     const query = Category.query()
 
     if(title) {
-      query.where('title', 'LIKE', `%${title}%`)
+      query.where('title', 'ILIKE', `%${title}%`)
     }
 
     const categories = await query.paginate(pagination.page, pagination.limit)
@@ -19,17 +19,38 @@ class CategoryController {
 
  
   async store ({ request, response }) {
+    try {
+      const { title, description, image_id } = request.all()
+      const category = await Category.create({ title, description, image_id })
+      return response.status(201).send(category) 
+    } catch (error) {
+      return response.status(400).send({
+        message: "Erro ao cadastrar!"
+      })
+    }
+   
   }
-
  
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, request, response, view }) {
+    const category = await Category.findOrFail(id)
+    return response.send(category)
   }
 
 
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+    const category = await Category.findOrFail(id)
+    const { title, description, image_id } = request.all()
+
+    category.merge({ title, description, image_id })
+    await category.save(category)
+    
+    return response.send(category)
   }
 
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const category = await Category.findOrFail(id)
+    await category.delete()
+    await response.status(204).send()
   }
 }
 
